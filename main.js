951a80d23f4a99660a75ac1b21abec48b1ab44d8
GUSTAVO VITOR DE PAULA SOUZA <25203636@CFP127ALUNO.LOCAL>
2026-02-02 11:19:59 -0300

    [feat] propriedade de mudar a cor do body adicionada ao projeto, com 14 cores em português

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
-nome = "Gustavo"
-idade = "17"
+'use strict'
+
+const botaoTrocarCor = document.getElementById('trocar-cor')
+
+const traducoes = {
+    "azul": "blue",
+    "vermelho": "red",
+    "verde": "green",
+    "amarelo": "yellow",
+    "tijolo": "firebrick",
+    "laranja": "orange",
+    "preto": "black",
+    "neve": "snow",
+    "rosa": "hotpink",
+    "turquesa": "turquoise",
+    "branco": "white",
+    "marrom": "brown",
+    "ciano": "cyan"
+
+}
+
+function trocarCor () {
+    const input = document.getElementById('cor')
+    const valor = input.value.trim().toLowerCase()
+    
+    const corFinal = traducoes[valor] || valor
+    
+    console.log("Tentando trocar para:", corFinal) // Isso aparece no F12
+    document.documentElement.style.setProperty('--cor-bg', corFinal)
+}
+
+botaoTrocarCor.addEventListener('click', trocarCor)
